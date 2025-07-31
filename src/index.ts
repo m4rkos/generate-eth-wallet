a8a17144212f24d4c0ff028bfc82a080e6dc16b6
marcos eduardo <marcos.j.eduardo@gmail.com>
2025-07-31 18:09:13 +0100

    created base structure and add type script

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// src/index.ts
+console.log("Generate ETH wallet");
+
+import { Wallet } from 'ethers';
+
+const wallet = Wallet.createRandom();
+console.log('Address:', wallet.address);
+console.log('Private Key:', wallet.privateKey);
+console.log('Mnemonic:', wallet.mnemonic?.phrase);
